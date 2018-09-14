d1a29dfaef07465f2826400cf6ca5dcf2c8e47bb
Ramzi El-Ramahi <ramzi.el12@gmail.com>
2018-09-14 16:15:41 -0500

    Connected to the standins API, formatted the table

--- src/Components/layout/Landing.js
+++ src/Components/layout/Landing.js
@@ -18,32 +18,76 @@ class Landing extends Component {
         header
       )
       .then(data => {
-        this.setState({ response: data.data });
-        // if (this.state.response.length > 0){
-        console.log(this.state.response.matches);
-        console.log(this.state.response);
+        this.setState({ schedule: data.data });
+        // if (this.state.schedule.length > 0){
+        console.log(this.state.schedule.matches);
+        console.log(this.state.schedule);
+        // }
+      });
+    axios
+      .get(
+        "http://api.football-data.org/v2/competitions/2021/standings",
+        header
+      )
+      .then(data => {
+        this.setState({ standings: data.data.standings[0].table });
+        // if (this.state.standings.length > 0){
+        console.log(this.state.standings.matches);
+        console.log("Standings", this.state.standings);
         // }
       });
   }
 
   render() {
-    if (!this.state.response) return <p className="loader">Loading....</p>;
+    if (!this.state.schedule) return <p className="loader">Loading....</p>;
+    if (!this.state.standings) return <p className="loader">Loading....</p>;
 
-    const card = this.state.response.matches.map(match => (
-      <div className="card">
-        <div className="homeTeam">
-          <h2>
-            {match.homeTeam.name} {match.score.fullTime.homeTeam}
-          </h2>
-          <div />
-        </div>
-        <div className="awayTeam">
-          <h2>
-            {match.awayTeam.name} {match.score.fullTime.awayTeam}
-          </h2>
+    const { schedule, standings } = this.state;
+
+    const tables = standings.map(standing => (
+      <div className="standings">
+        <div className="standingBody">
+          <div className="standingPosition">{standing.position}</div>
+          <img src={standing.team.crestUrl} alt="crest" className="crest" />
+          <div className="wins">{standing.won}</div>
+          <div className="draws">{standing.draw}</div>
+          <div className="lost">{standing.lost}</div>
+          <div className="plusminus">
+            {standing.goalsFor}/{standing.goalsAgainst}
+          </div>
+          <div className="GD">{standing.goalDifference}</div>
+          <div className="points">{standing.points}</div>
         </div>
       </div>
     ));
+
+    const card = schedule.matches.map(match => (
+      <div className="card">
+        <div className="cardHeader">
+          <div className="homeTeam">
+            <h2>{match.homeTeam.name}</h2>
+            <span className="score">{match.score.fullTime.homeTeam}</span>
+            <div />
+          </div>
+          <div className="awayTeam">
+            <h2>{match.awayTeam.name}</h2>
+            <span className="score">{match.score.fullTime.awayTeam}</span>
+          </div>
+        </div>
+        <div className="standingHeader">
+          <span className="standingPos">Pos.</span>
+          <span className="standingTeam">Team</span>
+          <span className="standingWins">Wins</span>
+          <span className="standingDraws">Draws</span>
+          <span className="standingLosses">Losses</span>
+          <span className="standingDiff">+/-</span>
+          <span className="standingGD">GD</span>
+          <span className="standingPoints">Points</span>
+        </div>
+        <div className="table">{tables}</div>
+      </div>
+    ));
+
     return <div className="cards">{card}</div>;
   }
 }
